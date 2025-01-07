105d2b22c49b566b4ab191035e3d4bfd0599df92
KOODIJONI <110822740+KOODIJONI@users.noreply.github.com>
2025-01-07 12:30:20 +0200

    build folder removed

--- viikkotehtava1/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_viikkotehtava1_FILE C:/oop/viikkotehtava1/build/viikkotehtava1.exe)
-set(__QT_DEPLOY_TARGET_viikkotehtava1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_viikkotehtava1_RUNTIME_DLLS C:/Qt2/6.7.3/llvm-mingw_64/bin/Qt6Core.dll)
